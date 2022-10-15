7862714ddf886aede2c168611067286d7df11f52
Amanibhavam <iam@defn.sh>
2022-10-15 17:34:59 +0000

    convert to cue

--- tf/control/main.tf
+++
@@ -1,9 +0,0 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "k3d-control"
-}
-
-module "devpod" {
-  source = "../m/devpod"
-  envs   = local.envs
-}
--- tf/control/variables.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  envs = {
-    "control" = {
-      host   = "k3d-control"
-      domain = "tiger-mamba.ts.net"
-    }
-  }
-}
