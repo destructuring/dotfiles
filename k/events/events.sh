eb4aab9f3ddad78058084214f9bde36acba01cd0
Amanibhavam <iam@defn.sh>
2022-10-19 00:28:27 +0000

    better instructions

--- k/events/events.sh
+++ k/events/events.sh
@@ -1,1 +1,6 @@
-~/bin/e kubectl get secret -n vc1 vc-vcluster -o yaml
+#!/usr/bin/env bash
+
+# pod create cm feh$(date +%s) --from-file=script=events.sh
+# pod logs -f -l events.argoproj.io/trigger=run-this
+
+~/bin/e kubectl get pods
