666af584fe3395918f3b4b924efd27919e7d2c3b
Amanibhavam <iam@defn.sh>
2022-10-25 06:35:28 +0000

    use aws scripts

--- k/events/events.sh
+++ k/events/events.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# control create cm feh$(date +%s) --from-file=script=events.sh
-# control logs -f -l events.argoproj.io/trigger=run-this
-
 set -exfu
 
 export AWS_REGION=us-west-1
 
-env | grep AWS_
-
-echo ~/bin/e aws ec2 describe-regions
-
-true
+~/bin/e aws ec2 describe-regions
