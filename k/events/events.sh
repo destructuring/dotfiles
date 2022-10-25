419ad1df1930cf94686979cb9e34c9525b93eaf2
Amanibhavam <iam@defn.sh>
2022-10-25 06:30:36 +0000

    trying to run a job with a serviceAccount

--- k/events/events.sh
+++ k/events/events.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# pod create cm feh$(date +%s) --from-file=script=events.sh
-# pod logs -f -l events.argoproj.io/trigger=run-this
+# control create cm feh$(date +%s) --from-file=script=events.sh
+# control logs -f -l events.argoproj.io/trigger=run-this
 
 set -exfu
 
-~/bin/e kubectl get pods
-echo meh
+export AWS_REGION=us-west-1
+
+env | grep AWS_
+
+echo ~/bin/e aws ec2 describe-regions
+
+true
