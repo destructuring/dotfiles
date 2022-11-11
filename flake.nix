439b529d0924f8fc393fc1a14a880a99594fd212
Amanibhavam <iam@defn.sh>
2022-11-11 17:21:17 +0000

    sugared

--- flake.nix
+++ flake.nix
@@ -1,53 +1,40 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    dev.url = "github:defn/pkg?dir=dev&ref=v0.0.11";
-    temporalite-pkg.url = "github:defn/pkg?dir=temporalite&ref=v0.0.4";
+    nixpkgs.url = github:NixOS/nixpkgs/22.05;
+    flake-utils.url = github:numtide/flake-utils;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.16;
+    wrapper.url = github:defn/pkg?dir=wrapper&ref=v0.0.16;
+    temporalite-pkg.url = github:defn/pkg?dir=temporalite&ref=v0.0.4;
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , dev
-    , temporalite-pkg
-    }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      temporalite = temporalite-pkg.defaultPackage.${system};
-    in
-    {
-      devShell =
-        pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            dev.defaultPackage.${system}
-            temporalite
-          ];
-        };
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        wrap = inputs.wrapper.wrap { other = inputs; inherit system; inherit pkgs; };
+        slug = "defn-app";
+        version = "0.0.1";
+        buildInputs = [
+        ];
+      in
+      rec {
+        devShell = wrap.devShell;
+        defaultPackage = pkgs.stdenv.mkDerivation
+          rec {
+            name = "${slug}-${version}";
 
-      defaultPackage =
-        with import nixpkgs { inherit system; };
-        stdenv.mkDerivation rec {
-          name = "${slug}-${version}";
+            dontUnpack = true;
 
-          slug = "defn-app";
-          version = "0.0.1";
+            installPhase = "mkdir -p $out";
 
-          dontUnpack = true;
+            propagatedBuildInputs = buildInputs;
 
-          installPhase = "mkdir -p $out";
-
-          propagatedBuildInputs = [
-          ];
-
-          meta = with lib;
-            {
+            meta = with pkgs.lib; {
               homepage = "https://defn.sh/${slug}";
               description = "nix golang / tilt integration";
               platforms = platforms.linux;
             };
-        };
-    });
+          };
+      }
+    );
 }
