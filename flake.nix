95fc2aef8aeea041b12789b80b8de12aa9ea1e63
Amanibhavam <iam@defn.sh>
2022-11-13 01:28:26 +0000

    updated

--- flake.nix
+++ flake.nix
@@ -1,36 +1,22 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.26;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.47;
   };
 
   outputs = inputs:
     inputs.dev.wrapper.flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
-        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; };
-        buildInputs = [
-        ];
         site = import ./config.nix;
+        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
+        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
       in
       with site;
       rec {
         devShell = wrap.devShell;
-        defaultPackage = pkgs.stdenv.mkDerivation
-          rec {
-            name = "${slug}-${version}";
-
-            dontUnpack = true;
-
-            installPhase = "mkdir -p $out";
-
-            propagatedBuildInputs = buildInputs;
-
-            meta = with pkgs.lib; with site; {
-              inherit homepage;
-              inherit description;
-              platforms = platforms.linux;
-            };
-          };
+        defaultPackage = wrap.bashBuilder {
+          src = ./.;
+          installPhase = "mkdir -p $out";
+        };
       }
     );
 }
