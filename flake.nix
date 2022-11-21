3a03790d57a2531373a0cdb7d7a8a45f47201518
Amanibhavam <iam@defn.sh>
2022-11-21 02:19:47 +0000

    use tf wrappers

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     c.url = "github:defn/pkg?dir=c&ref=v0.0.56";
     flyctl.url = "github:defn/pkg?dir=flyctl&ref=v0.0.56";
     argocd.url = "github:defn/pkg?dir=argocd&ref=v0.0.56";
+    tf.url = "github:defn/pkg?dir=tf&ref=v0.0.60";
   };
 
   outputs = inputs:
