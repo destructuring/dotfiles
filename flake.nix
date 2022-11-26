ea327407ddb9839910833cd4ea17305c8baed65c
Amanibhavam <iam@defn.sh>
2022-11-26 06:39:43 +0000

    remove deps from home

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
     dev.url = "github:defn/pkg?dir=dev&ref=v0.0.56";
-    c.url = "github:defn/pkg?dir=c&ref=v0.0.56";
-    flyctl.url = "github:defn/pkg?dir=flyctl&ref=v0.0.56";
-    argocd.url = "github:defn/pkg?dir=argocd&ref=v0.0.56";
-    tf.url = "github:defn/pkg?dir=tf&ref=v0.0.60";
   };
 
   outputs = inputs:
@@ -22,14 +18,7 @@
       handler = { pkgs, wrap, system }:
         rec {
           devShell = wrap.devShell;
-
-          defaultPackage = wrap.nullBuilder {
-            propagatedBuildInputs = with pkgs; [
-              kubernetes-helm
-              kustomize
-              terraform
-            ];
-          };
+          defaultPackage = wrap.nullBuilder { };
         };
     };
 }
