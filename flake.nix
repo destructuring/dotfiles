3ba0533a0f339ada8a1e5cb89be00e466dcb063a
Amanibhavam <iam@defn.sh>
2022-12-06 17:07:21 -0800

    whitespace

--- flake.nix
+++ flake.nix
@@ -1,23 +1,20 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.4?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.8?dir=dev;
   };
 
-  outputs = inputs:
-    inputs.dev.main {
-      inherit inputs;
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
 
-      config =
-        rec {
-          slug = "defn-app";
-          version_src = ./VERSION;
-          version = builtins.readFile version_src;
-        };
-
-      handler = { pkgs, wrap, system }:
-        rec {
-          devShell = wrap.devShell;
-          defaultPackage = wrap.nullBuilder { };
-        };
+    config = rec {
+      slug = "defn-app";
+      version_src = ./VERSION;
+      version = builtins.readFile version_src;
     };
+
+    handler = { pkgs, wrap, system }: rec {
+      devShell = wrap.devShell;
+      defaultPackage = wrap.nullBuilder { };
+    };
+  };
 }
