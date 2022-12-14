832ed42f4f569480da717409491d07a5e8bdc490
Amanibhavam <iam@defn.sh>
2022-12-13 18:52:51 -0800

    update defn/pkg/dev 0.0.12

--- flake.nix
+++ flake.nix
@@ -1,18 +1,19 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.8?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.12?dir=dev;
   };
 
   outputs = inputs: inputs.dev.main {
     inherit inputs;
+
+    src = ./.;
 
     config = rec {
       slug = "defn-app";
       version = builtins.readFile ./VERSION;
     };
 
-    handler = { pkgs, wrap, system }: rec {
-      devShell = wrap.devShell { };
+    handler = { pkgs, wrap, system, builders }: rec {
       defaultPackage = wrap.nullBuilder { };
     };
   };
