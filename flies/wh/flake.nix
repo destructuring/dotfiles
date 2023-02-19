50a915e4e507c5d24c723aafd36f9d290a3f3c40
amanibhavam <iam@defn.sh>
2023-02-19 08:00:17 +0000

    wh 0.0.2

--- flies/wh/flake.nix
+++ flies/wh/flake.nix
@@ -1,56 +1,47 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-    tired-proxy.url = github:defn/pkg/tired-proxy-0.0.4?dir=tired-proxy;
-    caddy.url = github:defn/pkg/caddy-2.6.2-5?dir=caddy;
-    webhook.url = github:defn/pkg/webhook-2.8.0?dir=webhook;
+    pkg.url = github:defn/pkg/0.0.166;
+    tired-proxy.url = github:defn/lib/tired-proxy-0.0.1?dir=cmd/tired-proxy;
+    caddy.url = github:defn/pkg/caddy-2.6.3-2?dir=caddy;
+    webhook.url = github:defn/pkg/webhook-2.8.0-3?dir=webhook;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
 
-    src = builtins.path { path = ./.; name = config.slug; };
-
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      version = builtins.readFile ./VERSION;
+    devShell = ctx: ctx.wrap.devShell {
+      devInputs = [
+        (defaultPackage ctx)
+      ];
     };
 
-    handler = { pkgs, wrap, system, builders }: rec {
-      defaultPackage = wrap.bashBuilder {
-        inherit src;
+    defaultPackage = ctx: ctx.wrap.bashBuilder {
+      inherit src;
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp nix-* $out/bin/
-          cp bin/* $out/bin/
-        '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp nix-* $out/bin/
+        cp bin/* $out/bin/
+      '';
 
-        propagatedBuildInputs = [
-          inputs.tired-proxy.defaultPackage.${system}
-          inputs.caddy.defaultPackage.${system}
-          inputs.webhook.defaultPackage.${system}
-          pkgs.bashInteractive
-          pkgs.curl
-          pkgs.git
-          pkgs.jq
-        ];
-      };
+      propagatedBuildInputs = [
+        inputs.tired-proxy.defaultPackage.${ctx.system}
+        inputs.caddy.defaultPackage.${ctx.system}
+        inputs.webhook.defaultPackage.${ctx.system}
+        ctx.pkgs.bashInteractive
+        ctx.pkgs.curl
+        ctx.pkgs.git
+        ctx.pkgs.jq
+      ];
+    };
 
-      packages = {
-        this-gen-key = pkgs.writeShellScriptBin "this-gen-key" ''
-          set -exfu
+    packages = ctx: {
+      this-gen-key = ctx.pkgs.writeShellScriptBin "this-gen-key" ''
+        set -exfu
 
-          nix-store --generate-binary-cache-key binarycache.example.com cache-priv-key.pem cache-pub-key.pem
-          chmod 600 cache-priv-key.pem
-        '';
-      };
-
-      devShell = wrap.devShell {
-        devInputs = with packages; [
-          this-gen-key
-        ];
-      };
+        nix-store --generate-binary-cache-key binarycache.example.com cache-priv-key.pem cache-pub-key.pem
+        chmod 600 cache-priv-key.pem
+      '';
     };
   };
 }
