731009118fc9ba42f9549d417dc6ce568051b796
amanibhavam <iam@defn.sh>
2023-02-19 07:32:46 +0000

    flies uses pkg.main

--- flies/flake.nix
+++ flies/flake.nix
@@ -1,17 +1,15 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
-    terraform.url = github:defn/pkg/terraform-1.3.8-0?dir=terraform;
-    kubernetes.url = github:defn/pkg/kubernetes-0.0.6?dir=kubernetes;
-    cloud.url = github:defn/pkg/cloud-0.0.1?dir=cloud;
+    pkg.url = github:defn/pkg/0.0.165;
+    kubernetes.url = github:defn/pkg/kubernetes-0.0.8?dir=kubernetes;
+    cloud.url = github:defn/pkg/cloud-0.0.3?dir=cloud;
+    terraform.url = github:defn/pkg/terraform-1.4.0-beta2-1?dir=terraform;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    config = rec {
+    config = {
       flies = [
         "brie"
         "so"
@@ -22,36 +20,44 @@
       ];
     };
 
-    # get a dev shell and default package
-    handler = { pkgs, wrap, system, builders, commands, config }: rec {
-      devShell = wrap.devShell {
-        devInputs = [ defaultPackage ];
-      };
-
-      defaultPackage = wrap.nullBuilder {
-        propagatedBuildInputs = wrap.flakeInputs ++ commands ++ (map (name: packages.${name}) config.flies);
-      };
-
-      # scripts
-      packages = pkgs.lib.genAttrs config.flies (name:
-        pkgs.writeShellScriptBin "${name}" ''
-          set -exfu
-
-          case "''${1:-}" in
-            build)
-              cd ./$(git rev-parse --show-cdup)/flies/${name}
-              time earthly --push --no-output +image --image=registry.fly.io/${name}:latest
-              docker pull registry.fly.io/${name}:latest
-              ;;
-            deploy)
-              cd ./$(git rev-parse --show-cdup)/flies/${name}
-              flyctl machine update -a ${name} --yes --dockerfile Dockerfile $(flyctl machine list -a ${name} --json | jq -r '.[].id')
-              ;;
-            *)
-              exec flyctl -a ${name} "$@"
-              ;;
-          esac
-        '');
+    devShell = ctx: ctx.wrap.devShell {
+      devInputs = [
+        (defaultPackage ctx)
+      ];
     };
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs =
+        let
+          flakeInputs = [
+            inputs.kubernetes.defaultPackage.${ctx.system}
+            inputs.cloud.defaultPackage.${ctx.system}
+            inputs.terraform.defaultPackage.${ctx.system}
+          ];
+        in
+        flakeInputs
+        ++ ctx.commands
+        ++ (map (name: (packages ctx).${name}) config.flies);
+    };
+
+    packages = ctx: ctx.pkgs.lib.genAttrs config.flies (name:
+      ctx.pkgs.writeShellScriptBin "${name}" ''
+        set -exfu
+
+        case "''${1:-}" in
+          build)
+            cd ./$(git rev-parse --show-cdup)/flies/${name}
+            time earthly --push --no-output +image --image=registry.fly.io/${name}:latest
+            docker pull registry.fly.io/${name}:latest
+            ;;
+          deploy)
+            cd ./$(git rev-parse --show-cdup)/flies/${name}
+            flyctl machine update -a ${name} --yes --dockerfile Dockerfile $(flyctl machine list -a ${name} --json | jq -r '.[].id')
+            ;;
+          *)
+            exec flyctl -a ${name} "$@"
+            ;;
+        esac
+      '');
   };
 }
